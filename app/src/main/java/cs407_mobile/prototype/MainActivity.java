b8ecb3122234c23a154d2d4bb3a51c4ea8b58e5e
blakeyp <blakeyp@hotmail.co.uk>
2017-10-14 16:42:09 +0100

    fixed to send successive messages on same connection

--- app/src/main/java/cs407_mobile/prototype/MainActivity.java
+++ app/src/main/java/cs407_mobile/prototype/MainActivity.java
@@ -3,7 +3,6 @@ package cs407_mobile.prototype;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.Socket;
-import java.net.UnknownHostException;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.AsyncTask;
@@ -22,42 +21,54 @@ public class MainActivity extends AppCompatActivity {
     private String message;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {   // on opening the app
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         textField = (EditText) findViewById(R.id.editText1); // reference to the text field
         button = (Button) findViewById(R.id.button1); // reference to the send button
 
+        // start a new thread to make a connection to the server socket
         MakeConnection makeConnection = new MakeConnection();
         makeConnection.execute();
 
-
-        // Button press event listener
+        // button press event listener
         button.setOnClickListener(new View.OnClickListener() {
 
             public void onClick(View v) {
-                Log.d("TEST", "Pressed button");
-                message = textField.getText().toString(); // get the text message on the text field
-                textField.setText(""); // Reset the text field to blank
-                SendMessage sendMessageTask = new SendMessage();
-                sendMessageTask.execute();
+            Log.d("DEBUG", "Pressed button");
+            message = textField.getText().toString();   // get the text message on the text field
+            textField.setText("");   // reset the text field to blank
+            SendMessage sendMessageTask = new SendMessage();
+            sendMessageTask.execute();
             }
         });
     }
 
+    @Override
+    protected void onDestroy() {   // on closing the app
+        super.onDestroy();
+        Log.d("DEBUG", "Closing stuff");
+        printwriter.close();   // doesn't seem to work ?? just blocks - maybe already closed on server?
+        try {
+            client.close();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+        Log.d("DEBUG", "Closed stuff");
+    }
 
     private class MakeConnection extends AsyncTask<Void, Void, Void> {
 
         @Override
         protected Void doInBackground(Void... params) {
             try {
-
-                client = new Socket("172.31.54.204", 80);   // connect to the server
+                client = new Socket("172.31.223.242", 80);   // connect to the server
                 printwriter = new PrintWriter(client.getOutputStream(), true);
-                printwriter.write("connected"); // write the message to output stream
-                Log.d("TEST", "Connection made woooo");
-
+                printwriter.println("connected");   // write message to output stream with EOL char
+                printwriter.flush();
+                Log.d("DEBUG", "Connection made");
             } catch (IOException e) {
                 e.printStackTrace();
             }
@@ -66,37 +77,15 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-
     private class SendMessage extends AsyncTask<Void, Void, Void> {
 
         @Override
         protected Void doInBackground(Void... params) {
-
-            Log.d("TEST", "Sending message");
-            printwriter.write(message); // write the message to output stream
-            //printwriter.flush();
-            Log.d("TEST", "Sent message");
-
+            Log.d("DEBUG", "Sending message");
+            printwriter.println(message);
+            printwriter.flush();
+            Log.d("DEBUG", "Sent message");
             return null;
-        }
-
-    }
-
-
-    @Override
-    protected void onDestroy() {
-
-        super.onDestroy();
-
-        Log.d("TEST", "Closing stuff");
-
-        printwriter.close();
-
-        try {
-            client.close(); // closing the connection
-        }
-        catch (IOException e) {
-            e.printStackTrace();
         }
 
     }
