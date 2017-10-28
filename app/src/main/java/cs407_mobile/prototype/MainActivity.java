c6b812f96b3744ba0da08f2e5253fdc5097bf6d2
blakeyp <blakeyp@hotmail.co.uk>
2017-10-28 18:30:31 +0100

    UI layout/margins/constraints fixed according to tutorial

--- app/src/main/java/cs407_mobile/prototype/MainActivity.java
+++ app/src/main/java/cs407_mobile/prototype/MainActivity.java
@@ -11,7 +11,6 @@ import android.widget.EditText;
 public class MainActivity extends AppCompatActivity {
 
     private EditText ipField;
-    private Button button;
     private Button buttonConnect;
 
     @Override
@@ -19,32 +18,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        ipField = (EditText) findViewById(R.id.editText);   // reference to the IP address text field
+        buttonConnect = (Button) findViewById(R.id.buttonConnect);   // reference to the connect button
 
-        ipField = (EditText) findViewById(R.id.editText); // reference to the text field
-        //button = (Button) findViewById(R.id.button1); // reference to the send button
-        buttonConnect = (Button) findViewById(R.id.buttonConnect); // reference to the connect button
-
-//        // button press event listener
-//        button.setOnClickListener(new View.OnClickListener() {
-//
-//            public void onClick(View v) {
-//            Log.d("DEBUG", "Pressed Jump button");
-//            //message = textField.getText().toString();   // get the text message on the text field
-//            //textField.setText("");   // reset the text field to blank
-//            connectionManager.sendMessage("jump");
-//            }
-//        });
-
-        // button press event listener
+        // on clicking connect button
         buttonConnect.setOnClickListener(new View.OnClickListener() {
 
             public void onClick(View v) {
-                Log.d("DEBUG", "Pressed Connect Button");
-                Log.d("DEBUG", ipField.getText().toString());
+                Log.d("DEBUG", "Pressed Connect button");
+                Log.d("DEBUG", "IP address: " + ipField.getText().toString());
                 openController(ipField.getText().toString());
-                //message = textField.getText().toString();   // get the text message on the text field
-                //textField.setText("");   // reset the text field to blank
-                // start a new thread to make a connection to the server socket
             }
         });
 
